315937e61e2f649ca0317c098b7a3b10728834a0
nielsvanderm <104632922+nielsvanderm@users.noreply.github.com>
2022-05-17 14:09:01 +0200

    Update DuikerTool.py

--- DuikerTool.py
+++ DuikerTool.py
@@ -173,7 +173,7 @@ def DuikerVisualisatie(Intreedweerstand, Manning, Uittreedweerstand,
         txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
     
         # get a font
-        fnt = ImageFont.load("arial.ttf", 15)
+        fnt = ImageFont.truetype("Helvetica", 15)
         # get a drawing context
         d = ImageDraw.Draw(txt)
     
